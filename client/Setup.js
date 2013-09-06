8dc53dd19ad1e84ec4408c3f8206e933095df103
Florens Douwes <floens@gmail.com>
2013-09-06 22:50:44 +0200

    Old comment.

--- client/Setup.js
+++ client/Setup.js
@@ -1,6 +1,4 @@
 // Shared code seperating is done this way.
-// This is at the beginning of loading other scripts because SpriteManager is called
-//   directly when scripts are loaded, and servers don't have SpriteManager.
 window.CLIENT = true;
 window.SERVER = false;
 
